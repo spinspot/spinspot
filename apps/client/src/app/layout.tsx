d38c36d0051e7b0b11327a70dfbe9e8b908a9280
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-05-17 09:22:49 -0400

    feat(client): :sparkles: added QueryContext and service sample

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import QueryContext from "./lib/query-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryContext>{children}</QueryContext>
+      </body>
     </html>
   );
 }
