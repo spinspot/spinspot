877f5e448a0382b8366fb77531f60d229eeb2bd4
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2024-05-17 17:23:49 -0400

    fix(general): :wrench: fixed tailwind config

--- packages/tailwindcss-config/tailwind.config.ts
+++ packages/tailwindcss-config/tailwind.config.ts
@@ -1,59 +1,8 @@
-import type { Config as DaisyConfig } from "daisyui";
-import type { Config as TailwindConfig } from "tailwindcss";
-
-const daisyuiConfig: DaisyConfig = {
-  darkTheme: "night",
-  themes: [
-    // {
-    //   spinspot: {
-    //     primary: "#02415a",
-    //     secondary: "#6f3a82",
-    //     accent: "#5b2bca",
-    //     neutral: "#427AA1",
-    //     "base-100": "#EBF2FA",
-    //     info: "#53a6ea",
-    //     success: "#0bad1e",
-    //     warning: "#ff9900",
-    //     error: "#d92020",
-    //   },
-    // },
-    "light",
-    "dark",
-    "cupcake",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "synthwave",
-    "retro",
-    "cyberpunk",
-    "valentine",
-    "halloween",
-    "garden",
-    "forest",
-    "aqua",
-    "lofi",
-    "pastel",
-    "fantasy",
-    "wireframe",
-    "black",
-    "luxury",
-    "dracula",
-    "cmyk",
-    "autumn",
-    "business",
-    "acid",
-    "lemonade",
-    "night",
-    "coffee",
-    "winter",
-    "dim",
-    "nord",
-    "sunset",
-  ],
-};
+import { Config as DaisyConfig } from "daisyui";
+import { Config as TailwindConfig } from "tailwindcss";
 
 const config: TailwindConfig = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [],
   theme: {
     extend: {
       backgroundImage: {
@@ -63,7 +12,23 @@ const config: TailwindConfig = {
       },
     },
   },
-  daisyui: daisyuiConfig,
+  daisyui: <DaisyConfig>{
+    themes: [
+      {
+        spinspot: {
+          primary: "#02415a",
+          secondary: "#6f3a82",
+          accent: "#5b2bca",
+          neutral: "#427aa1",
+          "base-100": "#ebf2fa",
+          info: "#53a6ea",
+          success: "#0bad1e",
+          warning: "#ff9900",
+          error: "#d92020",
+        },
+      },
+    ],
+  },
   plugins: [
     require("daisyui"),
     require("@tailwindcss/typography"),
