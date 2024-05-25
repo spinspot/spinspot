2107ea03e28894b4d91a533534584fa16b0224a2
raco45 <rcaceresortiz1@gmail.com>
2024-05-25 14:06:43 -0400

    fix(client): Card and dashboard styles

--- apps/client/src/app/dashboard/page.tsx
+++ apps/client/src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Card } from "@spin-spot/components";
+import { Button, Card} from "@spin-spot/components";
 import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
@@ -12,37 +12,46 @@ export default function Dashboard() {
   return (
     <div className="flex-grow">
       <div className="grid h-56 w-full items-center justify-items-center bg-black md:h-60">
-        <div className="font-quicksand text-center font-normal">
-          <h1 className="text-2xl">PING PONG</h1>
-          <h1 className="text-2xl">UNIMET</h1>
+        <div className="font-tittle text-center">
+          <h1 className="text-xl flex flex-col">
+            <span>PING PONG</span> 
+            <span>UNIMET</span>
+            </h1>
+          
         </div>
       </div>
       <div className="font-quicksand h-56  w-full pt-5 text-center font-normal md:h-60">
-        <h1 className="text-1xl h-10">¡Bienvenido a Spin-Spot! </h1>
+        <h2 className="text-xl pb-3">¡Bienvenido a Spin-Spot! </h2>
         <p className="px-6 pb-5 text-center">
           En esta página podrás realizar las reservas de las nuevas canchas de
           Ping Pong disponibles para los estudiantes.
         </p>
         <Button
-          className="btn btn-active bg-primary h-16 w-72 text-white"
+          className="btn-lg bg-primary"
           label="¿Estas listo para empezar a jugar?"
           labelSize="text-md"
           onClick={handleClick}
         />
       </div>
       <div className="font-quicksand w-full pt-10 text-center font-normal md:h-20">
-        <h1 className="h-10 text-2xl">Torneos activos</h1>
+        <h2 className="h-10 text-2xl">Torneos activos</h2>
       </div>
 
       <div className="carousel carousel-center w-full space-x-8  bg-inherit p-4 md:justify-center">
         <Card 
           label="Inicio: 11 de junio"
+          labelName="Torneo profesores"
+          labelButtom="Jugar"
           className="carousel-item"/>
         <Card 
           label="Inicio: 17 de julio"
+          labelName="Torneo Ingenieria"
+          labelButtom="Jugar"
           className="carousel-item"/>
         <Card 
           label="Inicio 3 de junio"
+          labelName="Torneo FACES"
+          labelButtom="Jugar"
           className="carousel-item"/>
         
       </div>
--- packages/components/src/cards/Card.tsx
+++ packages/components/src/cards/Card.tsx
@@ -2,16 +2,22 @@ import { Button } from "@spin-spot/components";
 import { cn } from "@spin-spot/utils";
 
 interface cardProps{
+  labelName?:string;
   label?: string;
   className?: string;
+  labelButtom?:string;
+  onClick?: () => void;
 }
 
 export function Card({
+    labelName,
     label, 
+    labelButtom,
+    onClick,
     className}:cardProps) {
   return (
     <>
-      <div className={cn("card w-72 shadow-xl", className)}>
+      <div className={cn("card w-72 shadow-xl break-all", className)}>
         <figure className="h-50">
           <img
             src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
@@ -19,13 +25,14 @@ export function Card({
           />
         </figure>
         <div className="card-body p-5">
-          <h2 className="card-title text-2xl font-semibold">Torneo</h2>
+          <h2 className="card-title text-2xl font-semibold">{labelName}</h2>
           <p>{label}</p>
           <div className="card-actions justify-start">
             <Button
-              className="btn-md btn-neutral"
-              label="Ver evento"
+              className="btn-md btn-primary"
+              label={(labelButtom)}
               labelSize="text-md"
+              onClick={(onClick)}
             />
           </div>
         </div>
