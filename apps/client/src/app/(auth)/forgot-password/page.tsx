e37106863caad65b431a1f6e5ba20d5bb6c07047
Fabrizio Spiotta <113402953+Faspiru@users.noreply.github.com>
2024-05-28 19:21:22 -0400

    fix(client): :art: Improving

--- apps/client/src/app/(auth)/forgot-password/page.tsx
+++ apps/client/src/app/(auth)/forgot-password/page.tsx
@@ -49,6 +49,7 @@ export default function ResetPassword() {
   };
 
   useScrollLock();
+
   return (
     <div className="absolute inset-0 z-40 flex items-center justify-center">
       <div className="mt-24 w-96 space-y-4 rounded-lg p-8 sm:mt-36">
