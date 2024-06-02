11ea4702bd0f3a9826903cc916dcb5fab03122d1
Fabrizio Spiotta <113402953+Faspiru@users.noreply.github.com>
2024-06-02 19:49:25 -0400

    fix(client): :lipstick: Fixing Styles

--- packages/components/src/reserves/SelectionSection.tsx
+++ packages/components/src/reserves/SelectionSection.tsx
@@ -10,34 +10,36 @@ interface SelectionSectionProps {
   resetInputs: (_length: number) => void;
 }
 
-export const SelectionSection: React.FC<SelectionSectionProps> = ({
+export function SelectionSection({
   options,
   optinosNo,
   setEventType,
   setIndumentary,
   resetInputs,
-}) => (
-  <div>
-    <div className="mt-6 flex flex-col items-center">
-      <h3 className="mr-1 text-lg">Seleccione modalidad: </h3>
-      <Pagination
-        labels={options}
-        size="sm"
-        onPageChange={(label) => {
-          setEventType(label ?? null);
-          resetInputs(label === "1V1" ? 1 : 3);
-        }}
-        className="btn-neutral mt-2 min-w-28 text-nowrap"
-      />
+}: SelectionSectionProps) {
+  return (
+    <div>
+      <div className="mt-6 flex flex-col items-center">
+        <h3 className="mr-1 text-lg">Seleccione modalidad: </h3>
+        <Pagination
+          labels={options}
+          size="sm"
+          onPageChange={(label) => {
+            setEventType(label ?? null);
+            resetInputs(label === "1V1" ? 1 : 3);
+          }}
+          className="btn-neutral mt-2 min-w-28 text-nowrap"
+        />
+      </div>
+      <div className="mt-6 flex flex-col items-center">
+        <h3 className="mr-1 text-lg">Indumentaria: </h3>
+        <Pagination
+          labels={optinosNo}
+          size="sm"
+          onPageChange={(label) => setIndumentary(label ?? null)}
+          className="btn-neutral mt-2 min-w-28 text-nowrap"
+        />
+      </div>
     </div>
-    <div className="mt-6 flex flex-col items-center">
-      <h3 className="mr-1 text-lg">Indumentaria: </h3>
-      <Pagination
-        labels={optinosNo}
-        size="sm"
-        onPageChange={(label) => setIndumentary(label ?? null)}
-        className="btn-neutral mt-2 min-w-28 text-nowrap"
-      />
-    </div>
-  </div>
-);
+  );
+}
