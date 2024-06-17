3af7019adcf694a42500578411af2f71160bd9fc
michellevillegasv <michelle.villegas@correo.unimet.edu.ve>
2024-06-17 18:37:28 -0400

    refactor(general): :art: Improving format of the code

--- apps/client/src/app/(user)/tournaments/page.tsx
+++ apps/client/src/app/(user)/tournaments/page.tsx
@@ -1,9 +1,7 @@
-export default function page() {;
-
+export default function page() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-5 text-lg font-bold">
       Tournaments Page 🚧
-
     </div>
   );
 }
