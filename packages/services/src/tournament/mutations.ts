9f4d99ac0ca3201e91b1ecaf22fc47453bae2080
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2024-06-17 18:28:38 -0400

    implemented join tournament mutation

--- packages/services/src/tournament/mutations.ts
+++ packages/services/src/tournament/mutations.ts
@@ -1,6 +1,8 @@
 import {
   ITournament,
   TCreateTournamentInputDefinition,
+  TUpdateTournamentParamsDefinition,
+  TUpdateTournamentInputDefinition
 } from "@spin-spot/models";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "../api";
@@ -26,3 +28,57 @@ export function useCreateTournament() {
     },
   });
 }
+
+export async function updateTournament({
+  _id,
+  ...input
+}: {
+  _id: TUpdateTournamentInputDefinition["_id"];
+} & TUpdateTournamentInputDefinition) {
+  const res = await api.put(`/tournaments/${encodeURIComponent(`${_id}`)}`, {
+    body: input,
+  });
+  const tournament: ITournament = await res.json();
+  return tournament;
+}
+
+export function useUpdateTournament() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["updateTournament"],
+    mutationFn: updateTournament,
+    onSuccess(data) {
+      queryClient.invalidateQueries({ queryKey: ["getTournaments"] });
+      queryClient.invalidateQueries({ queryKey: ["getTournament", data._id] });
+    },
+  });
+}
+
+export async function joinTournament({
+  _id,
+  ...input
+}: {
+  _id: TUpdateTournamentParamsDefinition["_id"];
+} & TUpdateTournamentInputDefinition) {
+  const res = await api.post(`/tournaments/${encodeURIComponent(`${_id}`)}/join`, {
+    body: input,
+  });
+  const tournament: ITournament = await res.json();
+  return tournament;
+}
+
+export function useJoinTournament() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["joinTournament"],
+    mutationFn: joinTournament,
+    onSuccess(data) {
+      queryClient.invalidateQueries({ queryKey: ["getTournaments"] });
+      queryClient.invalidateQueries({ queryKey: ["getTournament", data._id] });
+    },
+  });
+}
+
+
