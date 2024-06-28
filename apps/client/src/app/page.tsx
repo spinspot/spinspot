380ddb015f97e18a74ad3f02f53c825073c187fd
Fabrizio Spiotta <113402953+Faspiru@users.noreply.github.com>
2024-06-28 16:31:19 -0400

    fix(client): :bug: Fixing principal Page

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
 
   return (
     <LayoutWaves>
-      <div className="w-96 space-y-4 rounded-lg px-12 pb-2 pt-8">
-        <div className="flex flex-col items-center justify-center gap-9 pb-20">
+      <div className="space-y-4 rounded-lg px-12 pb-2 pt-8">
+        <div className="flex flex-col items-center justify-center gap-9">
           <SpinSpotIcon />
           <h2 className="dark:text-base-300 mb-1 text-center text-xl font-light  ">
             La nueva pagina de reservas de los unimetanos
