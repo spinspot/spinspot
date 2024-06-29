741cbf0889d95e63930d30b608e57026852ee5e1
Daniel Morillo <daniel.morillo@correo.unimet.edu.ve>
2024-06-29 19:14:22 -0400

    feat(admin) :sparkles: Tournament Creation Done

--- apps/admin/src/app/tournaments/create/page.tsx
+++ apps/admin/src/app/tournaments/create/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Button, Calendar, TextInput, SelectInput } from "@spin-spot/components";
 import { useRouter } from "next/navigation";
+import {useCreateTournament, useToast } from "@spin-spot/services";
 
 const eventTypeOptions = ["1V1", "2V2"];
 const tournamentTypeOptions = ["MEDIUM", "ADVANCED", "BEGINNER"];
@@ -10,51 +11,89 @@ const tournamentFormatOptions = ["LEAGUE", "ELIMINATION"];
 
 export default function Create() {
   const router = useRouter();
+  const { showToast } = useToast();
   const [startDate, setStartDate] = useState<Date | undefined>(undefined);
   const [endDate, setEndDate] = useState<Date | undefined>(undefined);
   const [maxTeams, setMaxTeams] = useState<number | undefined>(undefined);
+  const [maxPlayers, setMaxPlayers] = useState<number | undefined>(undefined);
   const [eventType, setEventType] = useState<string | undefined>(undefined);
   const [tournamentType, setTournamentType] = useState<string | undefined>(undefined);
   const [tournamentFormat, setTournamentFormat] = useState<string | undefined>(undefined);
-  const [prize, setPrize] = useState<number | undefined>(undefined);
+  const [prize, setPrize] = useState<number>(0);
+  const [tournamentName, setTournamentName] = useState<string>("");
+  const [tournamentDescription, setTournamentDescription] = useState<string>("");
+  const createTournament = useCreateTournament();
 
-  const handleSubmit = () => {
-    // Handle form submission logic here
-    console.log({
-      startDate,
-      endDate,
-      maxTeams,
-      eventType,
-      tournamentType,
-      tournamentFormat,
-      prize,
-    });
+  const handleCreate = () => {
+    if (!eventType || !tournamentType || !tournamentFormat || !startDate || !endDate || (eventType === "2V2" && maxTeams === undefined) || (eventType === "1V1" && maxPlayers === undefined) || prize === undefined || !tournamentName) {
+      showToast({
+        label: "Por favor, llena todos los campos",
+        type: "error"
+      });
+      return;
+    }
+    createTournament.mutate(
+      {
+        name: tournamentName,
+        description: tournamentDescription,
+        prize: prize.toString() + "$",
+        eventType: eventType as "1V1" | "2V2",
+        maxPlayers: maxPlayers,
+        maxTeams: maxTeams,
+        tournamentType: tournamentType as "MEDIUM" | "ADVANCED" | "BEGINNER",
+        tournamentFormat: tournamentFormat as "LEAGUE" | "ELIMINATION",
+        startTime: startDate,
+        endTime: endDate,
+        status: "OPEN",
+        owner: "665229652dc1249bcd4611b7",  // Cambiar esto a user?._id || " " cuando esté listo
+        location: "UNIMET",
+      }, {
+      onSuccess: () => {
+        showToast({
+          label: "Torneo creado exitosamente",
+          type: "success"
+        });
+        router.push("/tournaments");
+      },
+      onError: (error) => {
+        console.log(error);
+        showToast({
+          label: "Error al crear el torneo ", 
+          type: "error"
+        });
+    },
+  },
+    )
   };
+
 
   return (
     <div className="font-body flex-grow py-32">
       <div className="font-title font-bold text-center">
-        <h1 className="flex flex-col text-3xl">
+        <h1 className="flex flex-col text-3xl text-primary">
           <span>Crear</span>
           <span>Torneo</span>
         </h1>
       </div>
-      <div className="flex flex-col items-center mt-4 gap-y-4 max-w-md mx-auto ">
+      <div className="flex flex-col items-center mt-4 gap-y-4 max-w-md mx-auto text-primary ">
         <TextInput
-          topLeftLabel="Cantidad de equipos"
-          type="number"
-          min={2}
-          max={50}
-          placeholder="Cantidad de equipos"
-          value={maxTeams !== undefined ? maxTeams : ""}
-          onChange={(e) => setMaxTeams(e.target.value ? parseInt(e.target.value) : undefined)}
+          topLeftLabel="Nombre del torneo"
+          placeholder="Nombre del torneo"
+          value={tournamentName || ""}
+          onChange={(e) => setTournamentName(e.target.value)}
+        />
+        <TextInput
+          topLeftLabel="Descripción del torneo"
+          placeholder="Descripción del torneo"
+          value={tournamentDescription || ""}
+          onChange={(e) => setTournamentDescription(e.target.value)}
         />
         <TextInput
           topLeftLabel="Premio"
           type="number"
           placeholder="Premio"
-          value={prize !== undefined ? prize : ""}
-          onChange={(e) => setPrize(e.target.value ? parseFloat(e.target.value) : undefined)}
+          value={prize}
+          onChange={(e) => setPrize(parseInt(e.target.value))}
         />
         <SelectInput
           topLeftLabel="Tipo de evento"
@@ -62,6 +101,24 @@ export default function Create() {
           options={eventTypeOptions}
           value={eventType}
           onChange={(e) => setEventType(e.target.value)}
+        />
+        <TextInput
+          topLeftLabel={eventType === "1V1" ? "Cantidad de jugadores" : "Cantidad de equipos"}
+          type="number"
+          min={2}
+          max={50}
+          placeholder={eventType === "1V1" ? "Cantidad de jugadores" : "Cantidad de equipos"}
+          value={eventType === "1V1" ? maxPlayers || "" : maxTeams || ""}
+          onChange={(e) => {
+            const value = e.target.value ? parseInt(e.target.value) : undefined;
+            if (eventType === "1V1") {
+              setMaxPlayers(value);
+              setMaxTeams(undefined); 
+            } else {
+              setMaxTeams(value);
+              setMaxPlayers(undefined); 
+            }
+          }}
         />
         <SelectInput
           topLeftLabel="Tipo de torneo"
@@ -77,17 +134,19 @@ export default function Create() {
           value={tournamentFormat}
           onChange={(e) => setTournamentFormat(e.target.value)}
         />
-        <div className="flex flex-col items-center">
-          <span className="label-text text-base">Fecha de inicio</span>
+        <div className="flex flex-col items-center mt-2">
+          <span className="label-text text-2xl">Fecha de inicio</span>
           <Calendar onDateChange={setStartDate} endDate={endDate} />
         </div>
-        <div className="flex flex-col items-center">
-          <span className="label-text text-base">Fecha de finalización</span>
+        <div className="flex flex-col items-center mt-2">
+          <span className="label-text text-2xl">Fecha de finalización</span>
           <Calendar onDateChange={setEndDate} />
         </div>
-        <Button onClick={handleSubmit} label="Crear Torneo"></Button>
+        <Button onClick={handleCreate} label="Crear Torneo"></Button>
       </div>
     </div>
   );
 }
+
+
 
